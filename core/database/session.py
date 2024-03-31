fad424d9c8b08a28c6c6faa53ca65360ca7a0972
iyunakov <iyunakov@inbox.ru>
2024-03-31 22:16:42 +0300

    added context session

--- core/database/session.py
+++ core/database/session.py
@@ -1,5 +1,5 @@
-from typing import AsyncGenerator
 from contextlib import asynccontextmanager
+from typing import AsyncIterator, AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import sessionmaker
@@ -10,7 +10,12 @@ from core.database import engine
 async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 
+async def get_session() -> AsyncIterator[AsyncSession]:
+    async with async_session() as session:
+        yield session
+
+
 @asynccontextmanager
-async def get_session() -> AsyncGenerator[AsyncSession, None]:
+async def context_get_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
         yield session
