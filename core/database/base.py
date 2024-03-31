8e1ce50d244cf4be3b57a0e136e238dc5b1273a7
iyunakov <iyunakov@inbox.ru>
2024-03-31 22:15:14 +0300

    added created and updated to base model

--- core/database/base.py
+++ core/database/base.py
@@ -1,6 +1,14 @@
+from time import time
+
+from sqlalchemy import Column, Integer
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncAttrs
 
 
 class Base(AsyncAttrs, DeclarativeBase):
-    pass
+    created_at = Column(Integer, default=int(time()), nullable=False)
+    updated_at = Column(Integer, default=int(time()), onupdate=int(time()), nullable=False)
+
+    def to_dict(self, base_dict: dict = {}):
+        main_dict = {c.name: getattr(self, c.name) for c in self.__table__.columns}
+        return main_dict | base_dict
